0799fde4e21c768a25dfe57332dd8d28681d2358
fbonini <bonini.fr@gmail.com>
2026-07-05 23:54:02 -0300

    Landing 90% completada

---
+++ scripts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+//Expandir cards
+const cards = document.querySelectorAll('.servicio-card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+      const yaEstabaExpandida = card.classList.contains('expandida'); cards.forEach(c => c.classList.remove('expandida'));
+
+  if (!yaEstabaExpandida) {
+    card.classList.add('expandida');
+  }
+});
+});
+
+// Menú hamburguesa
+const btnHamburguesa = document.querySelector('.btn-hamburguesa');
+const navMobile = document.getElementById('navMobile');
+
+btnHamburguesa.addEventListener('click', () => {
+  btnHamburguesa.classList.toggle('abierto');
+  navMobile.classList.toggle('abierto');
+});
+
+// Cerrar menú al hacer click en un link
+navMobile.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    btnHamburguesa.classList.remove('abierto');
+    navMobile.classList.remove('abierto');
+  });
+});
